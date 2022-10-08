214b702592de317cf977d339e06218786789f696
Bryan Marfito <bjmarfito@yahoo.com>
2022-10-08 19:52:51 +0800

    Update prep_fault.m

--- prep_fault.m
+++ prep_fault.m
@@ -5,7 +5,7 @@ function [] = prep_fault(strike, dip, rake, len, wid, top_depth, vertices)
 % Important Notes:
 % 1. All units must be in degrees or meters 
 % 2. Right-hand rule is the convention for strike, dip, and rake measurements.
-% 3. Vertices should be a (2x2) matrix with the first row containing the x-coordinates (longitude) while the second row containing the y-coordinates (latitude).
+% 3. Vertices should be a (2x2) matrix with the first row containing the x-coordinates (longitude), and the second row containing the y-coordinates (latitude).
 % 4. Vertices should be in UTM coordinates. If the fault is due north, the first point is located south of the second point.
 % 5. Output filename is faultmodel.m
 %
