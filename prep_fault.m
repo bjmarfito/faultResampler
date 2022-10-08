850e2edecda792c6049eb4215ab37f084c108b52
bjmarfito <bryan@eos-bryanMarfito14268.local>
2022-10-08 15:18:09 +0800

    More fixes

--- prep_fault.m
+++ prep_fault.m
@@ -5,7 +5,7 @@ function [] = prep_fault(strike, dip, rake, len, wid, top_depth, vertices)
 % Important Notes:
 % 1. All units must be in degrees or meters 
 % 2. Right-hand rule is the convention for strike, dip, and rake measurements
-% 3. Vertices should be in UTM coordinates, If the fault is due north the first point is located south of the second point
+% 3. Vertices should be in UTM coordinates. If the fault is due north, the first point is located south of the second point.
 % 4. Output filename is faultmodel.m
 %
 %% Author: Bryan Marfito
@@ -18,4 +18,4 @@ faultstruct.L = len;
 faultstruct.zt = top_depth;
 faultstruct.vertices = vertices;
 
-save faultstruct faultmodel
+save faultmodel faultstruct
